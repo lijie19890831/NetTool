efcbb958fe95dfd11ffbe961d4bd1ff56dcd3891
lijie19890831 <290129130@qq.com>
2016-12-07 13:32:53 +0800

    Initial Commit

---
+++ netToolTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  netToolTest
+//
+//  Created by ios on 16/12/7.
+//  Copyright © 2016年 langzhijia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ netToolTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  netToolTest
+//
+//  Created by ios on 16/12/7.
+//  Copyright © 2016年 langzhijia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
